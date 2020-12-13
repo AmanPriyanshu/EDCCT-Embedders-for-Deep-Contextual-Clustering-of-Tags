ebbd62233c54ee0b40cde68af4cf47988ce3b026
Aman Priyanshu <aman.priyanshu@learner.manipal.edu>
2020-12-13 16:36:53 +0530

    product-wise-generator

---
+++ product_wise_generator.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import os
+from tqdm import tqdm
+
+data = pd.read_csv('reviews_Cell_Phones_and_Accessories_5.csv')
+f = data.columns
+data = data.values
+product_id = np.unique(data.T[1])
+os.system('mkdir product_wise')
+
+for p_id in tqdm(product_id):
+	p = []
+	for row in data:
+		if row[1] == p_id:
+			p.append(row)
+	p = pd.DataFrame(np.array(p))
+	p.columns = f
+	p.to_csv('./product_wise/'+str(p_id)+'.csv', index=False)
